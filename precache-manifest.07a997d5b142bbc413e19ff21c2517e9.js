2608cb00904bce74b3c308b0fa2467e601bdd912
Abraham Zilberblat <39259098+AZilberblat@users.noreply.github.com>
2020-04-06 21:22:56 +0300

    Updates

---
+++ precache-manifest.07a997d5b142bbc413e19ff21c2517e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6a8fd096d0eb5265dc44dbcf32607e",
+    "url": "/myPortfolioWeb/index.html"
+  },
+  {
+    "revision": "22b1d84e0150df80e255",
+    "url": "/myPortfolioWeb/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "44651eeef8d784fba709",
+    "url": "/myPortfolioWeb/static/css/main.c7bbc3ce.chunk.css"
+  },
+  {
+    "revision": "22b1d84e0150df80e255",
+    "url": "/myPortfolioWeb/static/js/2.8500f9b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myPortfolioWeb/static/js/2.8500f9b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44651eeef8d784fba709",
+    "url": "/myPortfolioWeb/static/js/main.9207d33f.chunk.js"
+  },
+  {
+    "revision": "eb16616b03174265bf1f",
+    "url": "/myPortfolioWeb/static/js/runtime-main.bcaf78db.js"
+  },
+  {
+    "revision": "add1be1e992c4cb146aa3fdd532ac4dd",
+    "url": "/myPortfolioWeb/static/media/aboutBg.add1be1e.jpg"
+  },
+  {
+    "revision": "e0b11766f83a589474ea0d7eb57c81fb",
+    "url": "/myPortfolioWeb/static/media/blockchain.e0b11766.gif"
+  },
+  {
+    "revision": "c0c0906bd8de634d4c128f646e621756",
+    "url": "/myPortfolioWeb/static/media/myImg.c0c0906b.jpg"
+  },
+  {
+    "revision": "c31e8f5a81f9f786c10269ccec6a7f7d",
+    "url": "/myPortfolioWeb/static/media/robbi.c31e8f5a.gif"
+  },
+  {
+    "revision": "b86a8c36e4fc0a7eb098d2fe9012c182",
+    "url": "/myPortfolioWeb/static/media/sally1.b86a8c36.gif"
+  }
+]);
